479a1b5485ed212e013fe72630e21bc8eb994a12
Matthew O. Smith <matt@m0smith.com>
2013-11-04 14:00:33 -0700

    Verify required cygwin packages are installed

--- install_win.sh
+++ install_win.sh
@@ -3,7 +3,10 @@
 ##
 #set -x
 
-cat <<EOF
+function cygwin_requires {
+    if [[ ! -e "/usr/bin/$1" ]]; then
+	echo "Missing $1"
+	cat <<EOF
 Before running this, be sure and install the latest cygwin:
       emacs, emacs-el, ctags, git, wget curl, openssh, zip, unzip, subversion.
 
@@ -13,9 +16,20 @@ To get git to work through a firewall try:
     export https_proxy=http://user:pass@host:port
 
 EOF
+	exit 1
+    fi
+}
 
 os=`uname -o`
 if [[ "$os" = "Cygwin" ]]; then 
+
+    cygwin_requires "emacs"
+    cygwin_requires "curl"
+    cygwin_requires "wget"
+    cygwin_requires "unzip"
+    cygwin_requires "zip"
+    cygwin_requires "ctags"
+    cygwin_requires "git"
 
     ## Setup home as per: http://stackoverflow.com/questions/225764/safely-change-home-directory-in-cygwin/10321615#10321615
     cd /
