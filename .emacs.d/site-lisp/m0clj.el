e2d8321a38276cac257a38712cc9f7338907c163
Matthew O. Smith <matt@m0smith.com>
2013-11-04 22:23:10 -0700

    Fleshed out the classpath resource search

--- .emacs.d/site-lisp/m0clj.el
+++ .emacs.d/site-lisp/m0clj.el
@@ -9,11 +9,52 @@
 ;;;Search a ZIP file: From http://stackoverflow.com/a/5428265/850252
 ;; Maybe use a ZipInputStream
 
-(defun m0clj-resource-search ()
+(defun m0clj-resource-init()
   (interactive)
-  (nrepl-send-string-sync "(defn m0clj-filenames-in-zip [filename]  (let [z (java.util.zip.ZipFile. filename)] (map #(.getName %) (enumeration-seq (.entries z)))))")
-  (nrepl-send-string-sync "(defn m0clj-classpath-urls [] (.getURLs (.getClassLoader (.getClass clojure-version))))")
+  (nrepl-send-string-sync 
+   "
+    (defprotocol EntryHandler
+       (entry-name [entry]))
+
+    (defrecord FileEntry [base file]
+       EntryHandler 
+       (entry-name [e] 
+          (let [b (.getPath base)
+                f (.getPath file)]
+            (.replace f b ""))))
+
+    (defrecord NewZipEntry [base file]
+       EntryHandler 
+       (entry-name [e] (.getName file)))
+
+    (defn m0clj-filenames-in-zip [filename]
+      (let [z (java.util.zip.ZipFile. filename)] 
+        (map #(NewZipEntry. filename %) (enumeration-seq (.entries z)))))
+
+    (defn m0clj-files-in [d]
+	 (map (fn [f] (FileEntry. d f))(filter #(not (.isDirectory %))(file-seq d))))
+
+    (defn m0clj-type-of [s]
+	(let [f (java.io.File. (.. s toURI))
+	      p (.getPath f)]
+	  (cond
+           (not (.exists f)) []
+	   (.isDirectory f) (m0clj-files-in f)
+	   (.endsWith p (str \j \a \r)) (m0clj-filenames-in-zip f)
+	   :else [s])))
+
+    (defn m0clj-classpath-urls [] 
+        (.getURLs (.getClassLoader (.getClass clojure-version))))
+ 
+    (defn m0clj-resources [] (map m0clj-type-of (m0clj-classpath-urls)))
+
+")
+
+   
 )
+
+;; (java.lang.System/getProperty "sun.boot.class.path")
+
 ;;#'user/filenames-in-zip
 ;;user> (def z "/media/xubuntu/D8D3-1C07/.m2/repository/clojurewerkz/archimedes/1.0.0-alpha5/archimedes-1.0.0-alpha5.jar")
 ;;#'user/z
