6482cc8586d3209eed20c43da4cbcbac3cf18621
Matthew O. Smith <matt@m0smith.com>
2013-11-05 12:04:24 -0700

    Added the ability to search the class path - required cider

--- .emacs.d/site-lisp/m0clj.el
+++ .emacs.d/site-lisp/m0clj.el
@@ -6,62 +6,22 @@
 ;;(nrepl-send-string-sync "(.getContextClassLoader (Thread/currentThread))")
 ;;(nrepl-send-string-sync "(.getURLs (.getClassLoader (.getClass clojure-version)))")
 
-;;;Search a ZIP file: From http://stackoverflow.com/a/5428265/850252
-;; Maybe use a ZipInputStream
-
-(defun m0clj-resource-init()
+(defun m0clj-resource-init ()
   (interactive)
-  (nrepl-send-string-sync 
-   "
-    (defprotocol EntryHandler
-       (entry-name [entry]))
-
-    (defrecord FileEntry [base file]
-       EntryHandler 
-       (entry-name [e] 
-          (let [b (.getPath base)
-                f (.getPath file)]
-            (.replace f b ""))))
-
-    (defrecord NewZipEntry [base file]
-       EntryHandler 
-       (entry-name [e] (.getName file)))
-
-    (defn m0clj-filenames-in-zip [filename]
-      (let [z (java.util.zip.ZipFile. filename)] 
-        (map #(NewZipEntry. filename %) (enumeration-seq (.entries z)))))
-
-    (defn m0clj-files-in [d]
-	 (map (fn [f] (FileEntry. d f))(filter #(not (.isDirectory %))(file-seq d))))
-
-    (defn m0clj-type-of [s]
-	(let [f (java.io.File. (.. s toURI))
-	      p (.getPath f)]
-	  (cond
-           (not (.exists f)) []
-	   (.isDirectory f) (m0clj-files-in f)
-	   (.endsWith p (str \j \a \r)) (m0clj-filenames-in-zip f)
-	   :else [s])))
-
-    (defn m0clj-classpath-urls [] 
-        (.getURLs (.getClassLoader (.getClass clojure-version))))
- 
-    (defn m0clj-resources [] (map m0clj-type-of (m0clj-classpath-urls)))
-
-")
-
-   
+  (nrepl-send-string-sync "(m0clj-classpath.tools/m0clj-init)")
 )
 
-;; (java.lang.System/getProperty "sun.boot.class.path")
+(defun m0clj-resource-find (s)
+  (interactive "sCamel-case:")
+  (message (plist-get 
+   (nrepl-send-string-sync (format "(map first (m0clj-classpath.tools/m0clj-resource-search \"%s\"))" s))
+   :value)))
 
-;;#'user/filenames-in-zip
-;;user> (def z "/media/xubuntu/D8D3-1C07/.m2/repository/clojurewerkz/archimedes/1.0.0-alpha5/archimedes-1.0.0-alpha5.jar")
-;;#'user/z
-;;user> (filenames-in-zip z)
-
-
-
+(defun m0clj-class-find (s)
+  (interactive "sCamel-case:")
+  (message (plist-get 
+   (nrepl-send-string-sync (format "(map (comp m0clj-path-to-full-class first) (m0clj-classpath.tools/m0clj-class-search \"%s\"))" s))
+   :value)))
 
 
 ;; Recursively generate tags for all *.clj files, 
