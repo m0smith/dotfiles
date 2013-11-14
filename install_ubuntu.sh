ec87711e0f6f238176296247b67044f0436b00fb
Matthew O. Smith <matt@m0smith.com>
2013-11-14 00:36:40 -0700

    Initial version

---
+++ install_ubuntu.sh
@@ -0,0 +1,14 @@
+#
+# Useful and necessary
+#
+
+sudo apt-get install emacs24 emacs24-el git ssh xterm  software-properties-common
+
+#
+# Java
+# See http://www.ubuntugeek.com/how-to-install-oracle-java-7-in-ubuntu-12-04.html
+#
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
