e3e9734529978dcc3e52b10b24f4723f6bad06e3
Matthew O. Smith <matt@m0smith.com>
2013-11-15 12:14:04 -0700

    Add tracing

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,6 @@
             [lein-clojars "0.9.1"]
             [lein-exec "0.3.1"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.3"]
+                 [org.clojure/tools.trace "0.7.5"]
                  [m0clj-classpath "0.1.5"]
                  [clj-stacktrace "0.2.6"]]}}
