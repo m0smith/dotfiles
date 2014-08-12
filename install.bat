313c56451f9acd8c733eb3f2dd4a653f8f91efc9
Matthew O. Smith <matt@m0smith.com>
2014-08-12 00:19:48 -0600

    Install.bat

---
+++ install.bat
@@ -0,0 +1,3 @@
+cscript mswindows\fetchcygwin.vbs
+c:\temp\setup-x86_64 -s ftp://mirrors.xmission.com -g -M -P  emacs,emacs-el,ctags,git,wget,curl,openssh,zip,unzip,subversion,dos2unix,gnupg,cvs,make,aspell,aspell-en
+C:\cygwin64\bin\bash.exe -l -c projects/dotfiles/install.sh
