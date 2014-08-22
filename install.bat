c55927dbe4c3cc3d6f5a5a336fc7e1bc7c875427
Matthew O. Smith <matt@m0smith.com>
2014-08-22 15:30:07 -0600

    Added the ability to rerun

--- install.bat
+++ install.bat
@@ -1,3 +1,58 @@
+
+SETLOCAL ENABLEEXTENSIONS
+
+
+IF "%ROOTDIR%"=="" set ROOTDIR=c:
+set CYGROOTDIR=%ROOTDIR%\cygwin64
+
+
+REM
+REM make sure c:\temp exists
+REM
+
+set A=c:\temp
+for %%i in (%A%) do set ATTR=%%~ai
+set DIRATTR=%ATTR:~0,1%
+if /I "%DIRATTR%" NEQ "d" mkdir %A%
+
+REM
+REM Download Cygwin to c:\temp
+REM
+
 cscript mswindows\fetchcygwin.vbs
-c:\temp\setup-x86_64 -s ftp://mirrors.xmission.com -g -M -P  emacs,emacs-el,ctags,git,wget,curl,openssh,zip,unzip,subversion,dos2unix,gnupg,cvs,make,aspell,aspell-en
-C:\cygwin64\bin\bash.exe -l -c projects/dotfiles/install.sh
+
+REM 
+REM Install required packages from Cygwin
+REM
+
+c:\temp\setup-x86_64 -R %CYGROOTDIR%  -g  -P  emacs,emacs-el,ctags,git,wget,curl,openssh,zip,unzip,subversion,dos2unix,gnupg,cvs,make,aspell,aspell-en
+
+REM
+REM Download and install dotfiles
+REM
+
+set A=%USERPROFILE%\projects
+for %%i in (%A%) do set ATTR=%%~ai
+set DIRATTR=%ATTR:~0,1%
+if /I "%DIRATTR%" NEQ "d" mkdir %A%
+
+set DF=%USERPROFILE%\projects\dotfiles
+for %%i in (%DF%) do set ATTR=%%~ai
+set DIRATTR=%ATTR:~0,1%
+if /I "%DIRATTR%"=="d" goto RUNINSTALL
+
+chdir %A%
+%CYGROOTDIR%\bin\git.exe clone  https://github.com/m0smith/dotfiles.git
+
+REM
+REM Run the dotfiles install
+REM
+
+:RUNINSTALL
+SET INSTALLSCRIPT=%DF%\install.sh
+for /F "usebackq delims==" %%f in (`c:\temp\hams\cygwin64\bin\cygpath.exe %INSTALLSCRIPT%`) do set IS2=%%f
+
+%CYGROOTDIR%\bin\chmod.exe +x %IS2%
+%CYGROOTDIR%\bin\bash.exe -l -c %IS2%
+
+REM http://stackoverflow.com/questions/138981/how-do-i-test-if-a-file-is-a-directory-in-a-batch-script
