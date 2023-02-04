e9ef1a9dbd4a7fc1e20f41941303ecffce5bb6c9
Matthew O. Smith <matt@m0smith.com>
2023-02-04 16:42:33 -0700

    Added npm setup

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,30 +1,31 @@
 ##
 ## Setup OS specific bash options
 
-link_with_backup2 ubuntu/.bashrc_ubuntu .profile.d/bashrc_ubuntu
+# link_with_backup2 ubuntu/.bashrc_ubuntu .profile.d/bashrc_ubuntu
 
-export JAD_EXE=jad
+# export JAD_EXE=jad
 
 #
 # Useful and necessary
 #
 
 
-sudo apt-get install emacs24 emacs24-el git bzr ssh xterm wget curl software-properties-common x11-apps unzip \
-                     cvs subversion texinfo
+sudo apt-get install git bzr ssh xterm wget curl software-properties-common x11-apps unzip \
+                     texinfo npm \
+                     # emacs26
 sudo apt-get autoremove
 
 #
 # Java
 # See http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
 #
-java_package=oracle-java8-installer
+# java_package=oracle-java8-installer
 
-dpkg -s "$java_package" >/dev/null 2>&1 || {
-    sudo add-apt-repository ppa:webupd8team/java
-    sudo apt-get update
-    sudo apt-get install oracle-java8-installer
-}
+# dpkg -s "$java_package" >/dev/null 2>&1 || {
+#     sudo add-apt-repository ppa:webupd8team/java
+#     sudo apt-get update
+#     sudo apt-get install oracle-java8-installer
+# }
 
 
 
