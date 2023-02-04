d8f3734bfc863d61acc9ba0a1c08059f971a8316
Matthew O. Smith <matt@m0smith.com>
2023-02-04 16:30:13 -0700

    Added git config

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global user.email "matt@m0smith.com"
+git config --global user.name "Matthew O. Smith"
